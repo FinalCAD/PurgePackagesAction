79ebb9be6c6d73ff9d4b20b294fe900ddc709a65
Maxime Charles <maxime.charles@finalcad.com>
2021-12-22 14:53:36 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# purge-packages
+# Purge NuGet packages action
+
+This action remove old NuGet packages.
+
+#
+## Outputs
+
+## `packagesDeleted`
+
+The list of packages removed.
+
+## Example usage
+
+uses: MaximeCharles/purge-packages@v0.1
---
+++ index.js
@@ -0,0 +1,117 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const githubToken = process.env.GITHUB_TOKEN;
+const ghClient = new github.getOctokit(githubToken);
+
+
+const getPackages = async (owner, repo) => {
+    let edges = await ghClient.graphql(`{
+        repository(owner: "${owner}", name:"${repo}"){
+            packages(first: 100, packageType: NUGET) {
+                edges {
+                  node {
+                    id
+                    name
+                    packageType
+                  }
+                }
+              }
+        }
+    }`, {});
+    edges = edges.repository.packages.edges;
+    return edges.map(p => {
+        return p.node.name;
+    });
+}
+
+const getVersions = async (owner, repo, package) => {
+    let edges = await ghClient.graphql(`{
+        repository(owner: "${owner}", name: "${repo}") {
+            packages(first: 1, packageType: NUGET, names: "${package}") {
+              edges {
+                node {
+                  versions(first: 100) {
+                    totalCount
+                    nodes {
+                      id
+                      version
+                    }
+                  }
+                }
+              }
+            }
+          }
+    }`, {});
+    edges = edges.repository.packages.edges;
+    return edges[0].node.versions.nodes.map(v => {
+        return {
+            id: v.id,
+            version: v.version
+        };
+    });
+}
+
+const deletePackage = async (version) => {
+    return new Promise((resolve, reject) => {
+        fetch(
+            'https://api.github.com/graphql',
+            {
+                method: 'post',
+                body: JSON.stringify({query: `mutation { deletePackageVersion(input:{packageVersionId:\"${version.id}\"}) { success }}`}),
+                headers: {
+                    'Accept': 'application/vnd.github.package-deletes-preview+json',
+                    'Authorization': `bearer ${githubToken}`
+                },
+            })
+            .then(res => res.json())
+            .then(json => {
+                core.debug(JSON.stringify(json));
+                resolve(json);
+            })
+            .catch(error => {
+                core.setFailed(`failed to delete ${JSON.stringify(version)}: ${JSON.stringify(error.message)}`);
+                reject();
+            });
+    })
+
+};
+
+const filterBy = (wildcard, str) => new RegExp('^' + wildcard.replace(/\*/g, '.*') + '$').test(str);
+
+const run = async () => {
+    const context = await github.context;
+    let owner = context.payload.repository.full_name.split('/')[0];
+    let repo = context.payload.repository.full_name.split('/')[1];
+
+    var packages = await getPackages(owner, repo);
+    versionToBeRemoved = [];
+    for (const package of packages) {
+        var versions = await getVersions(owner, repo, package);
+        preVersions = versions.filter(x => filterBy('*-*', x.version));
+        versionFinders = preVersions.map(p => p.version.split('-')[0])
+                                    .filter((value, index, self) => self.indexOf(value) === index);
+        for (const version of versionFinders) {
+            if (versions.some(p => p.version === version)) {
+                v = preVersions.filter(x => x.version.startsWith(version));
+                for (const d of v) {
+                    if (process.env.CI === 'true') {
+                        await deletePackage(d);
+                    } else {
+                        console.log(d);
+                    }
+                }
+                versionToBeRemoved = versionToBeRemoved.concat(v.map(o => {
+                    return `${package}:${o.version}`;
+                }));
+            }
+        }
+    }
+    core.setOutput('packagesDeleted', versionToBeRemoved.join(','));
+}
+
+try {
+    run();
+} catch (error) {
+    core.setFailed(error.message);
+}
